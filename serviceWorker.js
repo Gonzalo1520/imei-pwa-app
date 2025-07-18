2d20043dceb1d74c465758c0098566f510698bec
Goonzalo Pacheco Tapia <73792624+Gonzalo1520@users.noreply.github.com>
2025-07-18 11:31:36 -0500

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,11 +6,8 @@ self.addEventListener("install", e => {
     })
   );
 });
-
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
